8c58511ac298c16c7ab692fc71f47be3cb4efe29
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-07-09 15:07:15 +0800

    Firebase changes

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
+app.post("/send-mail", (req, res) => {
+  return res.status(200).send({ message: "Hello World" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is up in PORT: ${PORT}`);
 });
